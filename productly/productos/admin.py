9cb7b0097a480e3bb4abdbb59c996ea8ff5f0f77
emanuelcastanaza18 <ecastanazac@miumg.edu.gt>
2023-06-23 12:05:48 -0600

    Se realizaron modificaciones y avances al proyecto

--- productly/productos/admin.py
+++ productly/productos/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Producto, Categoria
+
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+
+class ProductoAdmin(admin.ModelAdmin):
+    exclude = ('creado_en', )
+    list_display = ('id', 'nombre', 'stock', 'puntaje', 'creado_en')
+
+
 
 # Register your models here.
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Categoria,CategoriaAdmin)
--- productly/productos/views.py
+++ productly/productos/views.py
@@ -1,6 +1,20 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
+
+#/productos
+
+
 def index(request):
-    return HttpResponse("Hola Futuro Ingeniero Emanuel, No te rindas sigue adelante")
+    # return HttpResponse("Hola Futuro Ingeniero Emanuel, No te rindas sigue adelante")
+    productos = Producto.objects.all().values() #Nos permite poder buscar absolutamente todos los registros que se encuentran dentro de la misma base de datos
+    # productos = Producto.objects.filter(puntaje=3) #Nos permite pasarle argumentos nombrados que van a permitir que nuestras consultas sean un poco mas complejas
+    # productos = Producto.objects.filter(puntaje__gte=3) #Nos permite pasarle argumentos nombrados que van a permitir que nuestras consultas sean un poco mas complejas
+    # productos = Producto.objects.filter(puntaje__lt=3) #Nos permite pasarle argumentos nombrados que van a permitir que nuestras consultas sean un poco mas complejas
+    # productos = Producto.objects.filter(puntaje__gt=3) #Nos permite pasarle argumentos nombrados que van a permitir que nuestras consultas sean un poco mas complejas
+    # productos = Producto.objects.get(id=1) #Nos permite buscar un elemento en particular
+    # productos = Producto.objects.get(pk=1) #Nos permite buscar un elemento en particular
+    # print(productos)
+    return JsonResponse(list(productos), safe=False)
