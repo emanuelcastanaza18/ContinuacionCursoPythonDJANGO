2db1a404e6b88cfa87360e723ef153032a10382e
emanuelcastanaza18 <ecastanazac@miumg.edu.gt>
2023-06-23 17:15:13 -0600

    Se realizaron mas avances en los cuales se instalo django.html para mejor funcionamiento

--- productly/productos/views.py
+++ productly/productos/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponse, JsonResponse
+from django.shortcuts import render, get_object_or_404
+
+from .forms import ProductForm
 from .models import Producto
 
 # Create your views here.
@@ -9,7 +11,7 @@ from .models import Producto
 
 def index(request):
     # return HttpResponse("Hola Futuro Ingeniero Emanuel, No te rindas sigue adelante")
-    productos = Producto.objects.all().values() #Nos permite poder buscar absolutamente todos los registros que se encuentran dentro de la misma base de datos
+    # productos = Producto.objects.all().values() #Nos permite poder buscar absolutamente todos los registros que se encuentran dentro de la misma base de datos
     # productos = Producto.objects.filter(puntaje=3) #Nos permite pasarle argumentos nombrados que van a permitir que nuestras consultas sean un poco mas complejas
     # productos = Producto.objects.filter(puntaje__gte=3) #Nos permite pasarle argumentos nombrados que van a permitir que nuestras consultas sean un poco mas complejas
     # productos = Producto.objects.filter(puntaje__lt=3) #Nos permite pasarle argumentos nombrados que van a permitir que nuestras consultas sean un poco mas complejas
@@ -17,4 +19,65 @@ def index(request):
     # productos = Producto.objects.get(id=1) #Nos permite buscar un elemento en particular
     # productos = Producto.objects.get(pk=1) #Nos permite buscar un elemento en particular
     # print(productos)
-    return JsonResponse(list(productos), safe=False)
+    # return JsonResponse(list(productos), safe=False)
+
+    #Plantillas 
+    # productos = Producto.objects.all()
+
+    # return render(
+    #     request, 
+    #     'index.html', 
+    #     context={'productos': productos,}    
+    # )
+
+    #Compartiendo plantilas
+        # productos = Producto.objects.all()
+
+        # return render(
+        #      request, 
+        #      'index1.html', 
+        #      context={'productos': productos,}    
+        #  )
+
+    #Parametros urls
+    productos = Producto.objects.all()
+
+    return render(
+        request, 
+        'index.html', 
+        context={'productos': productos,}    
+    )
+
+# def detalle(requests, producto_id):
+#     # return HttpResponse(f"Este es el producto {producto_id}")
+#     try:
+#         producto = Producto.objects.get(id=producto_id)
+
+#         return render(
+#             requests, 
+#             'detalle.html',
+#             context={'producto': producto})
+#     except Producto.DoesNotExist:
+#         raise Http404()
+
+#Otra manera de hacerlo
+def detalle(requests, producto_id):   
+        producto = get_object_or_404(Producto, id=producto_id)
+
+        return render(
+            requests, 
+            'detalle.html',
+            context={'producto': producto})
+def formulario(request):
+    form = ProductForm()
+
+    return render(
+         request,
+         'producto_form.html',
+         {'form': form}
+    )
+    
+    
+    
+    
+    
